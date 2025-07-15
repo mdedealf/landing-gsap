1cef9095a5123704b9bedf474ac34ad10af0c66f
mdedealf <dede.alfaruqq@gmail.com>
2025-07-15 18:57:31 +0700

    feat/hero : adding hero section content and animated with gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { ScrollTrigger } from "gsap/all";
 import NavBar from "./components/NavBar";
+import HeroSection from "./sections/HeroSection";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar />
+      <HeroSection />
+      <div className="h-dvh border-red-500"></div>
     </main>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
+      <img
+        src="public\images\nav-logo.svg"
+        alt="nav-logo"
+        className="md:w-24 w-20"
+      />
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
