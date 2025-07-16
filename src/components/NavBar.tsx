ef3de792e10997a4e5e5d5fed787a150fe905db4
mdedealf <dede.alfaruqq@gmail.com>
2025-07-16 13:31:13 +0700

    chore : adding scroll to top when clicking logo, add meta and web name

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,16 @@
 const NavBar = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
-      <img src="/images/nav-logo.svg" alt="nav-logo" className="md:w-24 w-20" />
+      <img
+        src="/images/nav-logo.svg"
+        alt="nav-logo"
+        className="md:w-24 w-20 cursor-pointer"
+        onClick={scrollToTop}
+      />
     </nav>
   );
 };
